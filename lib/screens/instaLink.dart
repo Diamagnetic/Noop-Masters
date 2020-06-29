e0e8e2716255be4741bbcd924cfbb182cdca1a24
Prajwal Kolte <prajwalkolte3@gmail.com>
2020-06-29 18:36:47 +0530

    done

--- lib/screens/instaLink.dart
+++ lib/screens/instaLink.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vjcommittee/constants.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LinkCreator extends StatelessWidget {
   LinkCreator({@required this.committeeLink});
@@ -9,26 +8,23 @@ class LinkCreator extends StatelessWidget {
   bool showSpinner = false;
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: showSpinner,
-      child: Card(
-        color: kAppbarBackgroundColour,
-        elevation: 20,
+    return Card(
+      color: kAppbarBackgroundColour,
+      elevation: 20,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: FlatButton(
+        onPressed: _launchURL(committeeLink),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(100),
         ),
-        child: FlatButton(
-          onPressed: _launchURL(committeeLink),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100),
-          ),
-          splashColor: Colors.grey.withOpacity(.3),
-          child: Center(
-            child: Text(
-              'SOCIAL',
-              style: kCardTitleTextStyle,
-              textAlign: TextAlign.center,
-            ),
+        splashColor: Colors.grey.withOpacity(.3),
+        child: Center(
+          child: Text(
+            'SOCIAL',
+            style: kCardTitleTextStyle,
+            textAlign: TextAlign.center,
           ),
         ),
       ),
